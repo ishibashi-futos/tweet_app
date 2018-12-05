0b857891c642336615ed35de4f3a647089d1cfd1
bash <rf6447.i.f@gmail.com>
2018-12-06 01:02:45 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'about'
-  get 'top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "top" => "home#top"
+  # aboutアクションへのルーティングを設定してください
+  get "about" => "home#about"
 end
