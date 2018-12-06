139fc528121550274aaf3527495d02645eea851f
bash <rf6447.i.f@gmail.com>
2018-12-06 16:15:27 +0900

    それぞれの投稿ページへのルーティングを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,8 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  
+  # showアクションを追加してください
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  get 'posts/' => 'posts#index'
+  get 'posts/index' => 'posts#index'
+  get 'posts/:id' => 'posts#show'
   get "/" => "home#top"
   # aboutアクションへのルーティングを設定してください
   get "about" => "home#about"
