d3ff68597bc5e9c6d237ac3e03b5f4ef5f778c3d
bash <rf6447.i.f@gmail.com>
2018-12-06 16:30:57 +0900

    投稿フォームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'posts/' => 'posts#index'
   get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
   get 'posts/:id' => 'posts#show'
   get "/" => "home#top"
   # aboutアクションへのルーティングを設定してください
