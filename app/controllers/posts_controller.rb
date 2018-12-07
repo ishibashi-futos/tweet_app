aa21fc217677b4f5bd1bceed5f97410cec49256b
bash <rf6447.i.f@gmail.com>
2018-12-08 01:11:48 +0900

    投稿のバリデーションチェック、エラーメッセージ、renderによるエラー時の編集の巻き戻しを追加。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,18 @@ class PostsController < ApplicationController
   end
   
   def new
+    @post = Post.new
   end
   
   def create
     @post = Post.new(content: params[:content])
-    @post.save
-    redirect_to("/posts/index")
+    if @post.save
+      # 変数flash[:notice]に、指定されたメッセージを代入してください
+      flash[:notice] = "投稿を作成しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
   end
   
   def edit
@@ -23,16 +29,21 @@ class PostsController < ApplicationController
   def update
     @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
-    @post.save
-    redirect_to("/posts/index")
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
+    end
   end
   
   def destroy
-    # destroyアクションの中身を作成してください
     @post = Post.find_by(id: params[:id])
-    @post.destroy
-    
-    redirect_to("/posts/index")
+    if @post.destroy then
+    # 変数flash[:notice]に、指定されたメッセージを代入してください
+      flash[:notice] = "投稿を削除しました"
+      redirect_to("/posts/index")
+    end
   end
   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    # contentカラムに対して、文字数を制限するためのバリデーションを追加してください
+    validates :content, {
+      presence: true,
+      length: { maximum: 140 }
+    }
 end
