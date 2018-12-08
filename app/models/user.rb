101bcd8932fbffcd693c17962669001740e595fb
ishibashi.futos <ishibashi.futos@jp.fujitsu.com>
2018-12-09 03:31:05 +0900

    ユーザ管理機能の追加

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+
+  # nameカラムに関するバリデーションを作成してください
+  validates :name, presence: true
+  # emailカラムに関するバリデーションを作成してください
+  validates :email, {
+    uniqueness: true,
+    presence: true
+  }
+  
+end
